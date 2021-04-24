6d5926bc6c9eb216eaba95b518c55a6ab8745efb
drojas-reservo <drojas@reservo.cl>
2021-04-24 19:51:35 -0400

    add: menu list view

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# Django
+from django.views.generic.list import ListView
 
-# Create your views here.
+# Models
+from .models import (
+    Menu,
+    MenuOption,
+    MenuOptionSelection,
+)
+
+
+class MenuListView(ListView):
+    """
+    View for displaying a list of menus by date.
+    """
+    model = Menu
+    template_name = 'menus/menu_list.html'
+    ordering = ['date']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.prefetch_related('options')
