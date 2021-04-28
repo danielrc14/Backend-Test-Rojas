cc7982a16eb7d09fe35bf07101d9a6fe6b756ad2
drojas-reservo <drojas@reservo.cl>
2021-04-28 13:28:04 -0400

    fix: fixes

--- backend_test/settings.py
+++ backend_test/settings.py
@@ -241,6 +241,7 @@ LOGGING = {
 }
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 AUTH_USER_MODEL = 'users.User'
 
--- users/slack_client.py
+++ users/slack_client.py
@@ -3,15 +3,19 @@ from slack_sdk.web import WebClient
 
 
 def send_slack_message(message, channel):
-    slack_web_client = WebClient(token=settings.SLACK_SDK_OAUTH_TOKEN)
-    if channel[0] != '@':
-        channel = '@' + channel
-    response = slack_web_client.chat_postMessage(
-        channel=channel,
-        username=settings.SLACK_USERNAME,
-        icon_emoji=':robot_face:',
-        text=message,
-    )
-    if not response.status_code == 200 or response.data.get('ok') is not True:
-        # Here there should be code to manage errors, like logs, etc.
-        pass
+    """
+    Use slack api to send a message to the desired channel
+    """
+    if channel:
+        slack_web_client = WebClient(token=settings.SLACK_SDK_OAUTH_TOKEN)
+        if channel[0] != '@':
+            channel = '@' + channel
+        response = slack_web_client.chat_postMessage(
+            channel=channel,
+            username=settings.SLACK_USERNAME,
+            icon_emoji=':robot_face:',
+            text=message,
+        )
+        if not response.status_code == 200 or response.data.get('ok') is not True:
+            # Here there should be code to manage errors, like logs, etc.
+            pass
